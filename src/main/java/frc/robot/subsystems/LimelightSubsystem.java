5dbeb50e70f7e4b61ec87743b5d852c51e3f879c
ICGStudios <46583403+ICGStudios@users.noreply.github.com>
2020-01-17 11:01:26 -0800

    d

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import static frc.robot.OI.*;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+public class LimelightSubsystem extends Subsystem {
+
+    public LimelightSubsystem() {
+    }
+
+
+    public void initDefaultCommand() {
+
+    }
+
+}
--- src/main/java/frc/robot/subsystems/ServoSubsystem.java
+++ src/main/java/frc/robot/subsystems/ServoSubsystem.java
@@ -16,18 +16,19 @@ public class ServoSubsystem extends Subsystem {
 
 
     public void initDefaultCommand() {
-        servo0.setAngle(90);
+        servo0.setAngle(45);
     }
 
     public void rotateArm(){
 
         if(rightJoystick.getRawButton(5) == true){
-            if(servo0.getAngle() > 0 || servo0.getAngle() < 180)
+            System.out.println("bitch");
+            // if(servo0.getAngle() > 0 || servo0.getAngle() < 180)
                 servo0.setAngle(servo0.getAngle() + 5);
         }
 
         if(rightJoystick.getRawButton(3) == true){
-            if(servo0.getAngle() > 0 || servo0.getAngle() < 180)
+            // if(servo0.getAngle() > 0 || servo0.getAngle() < 180)
                 servo0.setAngle(servo0.getAngle() - 5);
         }
 
