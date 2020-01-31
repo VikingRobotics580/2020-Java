58fa0372d8821b1f564a9bbdb04045e894f94b9b
ICGStudios <46583403+ICGStudios@users.noreply.github.com>
2020-01-30 16:32:31 -0800

    h

--- src/main/java/frc/robot/subsystems/BallIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallIntakeSubsystem.java
@@ -11,18 +11,22 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class BallIntakeSubsystem extends Subsystem {
 
-    WPI_TalonSRX ballIntake;
+    WPI_TalonSRX ballIntakeBack;
+    WPI_TalonSRX ballIntakeFront;
 
     public BallIntakeSubsystem() { //Constructor
 
-        ballIntake = new WPI_TalonSRX(TLN_BIT); //Identifies port of talon/motor
+        ballIntakeBack = new WPI_TalonSRX(TLN_BIT_BAK); //Identifies port of talon/motor
+        ballIntakeFront = new WPI_TalonSRX(TLN_BIT_FRT); //Identifies port of talon/motor
+
 
     }
 
 
     public void initDefaultCommand() { //Runs on startup
 
-        ballIntake.set(0); //Sets speed to 0 at start
+        ballIntakeBack.set(0); //Sets speed to 0 at start
+        ballIntakeFront.set(0); //Sets speed to 0 at start
         setDefaultCommand(new BallIntakeCommand()); //Variant is REQUIRED for subsystems
 
     }
@@ -30,10 +34,12 @@ public class BallIntakeSubsystem extends Subsystem {
     public void rotateIntake(){ //Runs intermitantly after start
 
         if(BIT_Pul)
-            ballIntake.set(BIT_Spd); //Set speed when button 6 is pressed
+            ballIntakeBack.set(BIT_Spd); //Set speed when button 6 is pressed
+            ballIntakeFront.set(BIT_Spd);
 
         if(BIT_Stp)
-            ballIntake.set(0); //Set speed to 0 when button 4 is pressed
+            ballIntakeBack.set(0); //Set speed to 0 when button 4 is pressed
+            ballIntakeFront.set(0); //Set speed to 0 when button 4 is pressed
 
 
     }
